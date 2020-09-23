55e11e553a94a5d70605dfd6412c990c2d31ec5d
Carlos Perello <70109918+carlosaccp@users.noreply.github.com>
2020-09-23 19:01:13 +0100

    Update stltop2.0.py
    Changed the scraper from 6 to itemppage

--- stltop2.0.py
+++ stltop2.0.py
@@ -1,4 +1,7 @@
-# Created by carlosaccp all rights reserved. The following code was written in MacOS Catalina 10.15.16
+# Created by carlosaccp all rights reserved. The following code was written in MacOS Catalina 10.15.1
+
+
+
 
 # The aim of this program is to scrape designer data from STLTop and store it in an sqlite file so it can be uploaded to Metabase
 # THIS PROGRAM IS MEANT TO BE RAN EVERY 24 HOURS, IF NOT THE DATA GATHERED IS USELESS!
@@ -20,7 +23,7 @@ from selenium.webdriver.chrome.options import Options
 # Create a date variable and set it to today's date and time
 date = datetime.date(datetime.now())
 
-# Define the options for when the chromedriver opens, which for me is set the window size to my monitor's size (fullscreen not working for some reason)
+# Define the options for when the chromedriver opens, which for me is set the window size to my monitor's size 
 chrome_options = Options()
 chrome_options.add_argument("window-size=1680,1050")
 
@@ -30,7 +33,7 @@ driver = webdriver.Chrome(executable_path=r"./chromedriver", options=chrome_opti
 driver.get("https://www.stltop.com/")
 
 # Things start to become specific to www.stltop.com, and may change a considerable amount when scraping other websites
-# For context, I am interested in scraping all of the designers listed in STLTop's names and patron numbers, and there are 6 designers lister per page
+# For context, I am interested in scraping all of the designers listed in STLTop's names and patron numbers, and there are itemppage designers lister per page
 
 # My preferred way of location elements is to locate them by XPath, but there are many other ways to do it such as locating it by id, name, tag name...
 
@@ -43,15 +46,17 @@ elements = int(driver.find_element_by_xpath(r"/html/body/section[2]/div/div[1]/d
 patronlist = []
 # Create a pgcounter variable which will be useful later on to count how many pages we have cycled through
 pgcounter = 1
+# Create a itemppage variable which represents how many items are displayed in a given page
+itemppage = 9
 
 #Create a for loop that loops from when number == 1 to when number == elements
 for number in range (1 , elements+1):
-    # Redefine the counter variable as number % 6. This means that we will know what element of a page the program is scraping as there are 6 elements per page.
-    counter = number % 6
-    # If counter != 0 <==> if the last element of the page hasn't been reached and pgcounter != math.floor(elements/6+1) <==> we are not on the last page, so
-    # we need to scrape 6 times, not any less as on the last page there is a 5/6 chance that there are < 6 elements, so we need to find a way to find how many
+    # Redefine the counter variable as number % itemppage. This means that we will know what item of the page the program is scraping as there are itemppage items per page.
+    counter = number % itemppage
+    # If counter != 0 <==> if the last element of the page hasn't been reached and pgcounter != math.floor(elements/(itemppage+1)) <==> we are not on the last page, so
+    # we need to scrape itemppage times, not any less as on the last page there is a itemppage-1/itemppage chance that there are < itemppage elements, so we need to find a way to find how many
     # elements are on the last page to only scrape said elements, not more as that leads to error
-    if (counter != 0) and pgcounter != math.floor(elements/6 + 1):
+    if (counter != 0) and pgcounter != math.floor(elements/(itemppage+1)):
             # Locate the name by xpath and store it in a variable. Note that I have formatted the XPath; this is because the XPath is correlated to the element 
             # number, so we can format the counter (which tells us which element of a page we are scraping) into the XPath to scrape the nth element.
             name = driver.find_element_by_xpath('/html/body/section[2]/div/div[4]/div[{}]/div[2]/div/div[1]/div/div/h5'.format(counter)).text
@@ -62,14 +67,14 @@ for number in range (1 , elements+1):
             # Append the info array to the patronlist array
             patronlist.append(sp.array(info))
             
-    # Else if counter == 0 <==> we are on the 6th element of the page <==> we need to go onto the next page after scraping this element. Moreover
-    # pgcounter != math.floor(elements/6 + 1) <==> we are not on the last page so we don't need to worry about having < 6 elements in the page
-    elif (counter == 0) and pgcounter != math.floor(elements/6 + 1):
-            # Locate the designer's name, same as before except that this time as we are on the 6th element we don't need to format the counter 
-            # as counter == 6 always
-            name = driver.find_element_by_xpath('/html/body/section[2]/div/div[4]/div[6]/div[2]/div/div[1]/div/div/h5').text
+    # Else if counter == 0 <==> we are on the itemppage-th element of the page <==> we need to go onto the next page after scraping this element. Moreover
+    # pgcounter != math.floor(elements/(itemppage+1)) <==> we are not on the last page so we don't need to worry about having < itemppage elements in the page
+    elif (counter == 0) and pgcounter != math.floor(elements/(itemppage+1)):
+            # Locate the designer's name, same as before except that this time as we are on the itemppage-th element we don't need to format the counter 
+            # as counter == itemppgage always
+            name = (driver.find_element_by_xpath('/html/body/section[2]/div/div[4]/div[{}]/div[2]/div/div[1]/div/div/h5'.format(itemppage)).text
             # Locate the designers' patrons with the same twist as the previous line, no formatting this time
-            patrons = [int(s) for s in driver.find_element_by_xpath('/html/body/section[2]/div/div[4]/div[6]/div[2]/div/div[1]/div/div/p').text.split() if s.isdigit()][0]
+            patrons = [int(s) for s in (driver.find_element_by_xpath('/html/body/section[2]/div/div[4]/div[{}]/div[2]/div/div[1]/div/div/h5'.format(itemppage)).text.split() if s.isdigit()][0]
             # Create an array with all of the designer's info we want
             info = [date, name, patrons]
             # Append the info array containing the designer's info to the patronlist array
@@ -82,8 +87,8 @@ for number in range (1 , elements+1):
             # Call fuckcookies again as we are navigating to another page where we might be faced with another 'Allow cookies' prompt
             fuckcookies
     
-    # Else if pgcounter == math.floor(elements/6 + 1) <==> we are on the last page
-    elif pgcounter == math.floor(elements/6 + 1):
+    # Else if pgcounter == math.floor((elements/itemppage) + 1) <==> we are on the last page
+    elif pgcounter == math.floor((elements/itemppage) + 1):
             # Find the designer's name by using counter and formatting it to the XPath
             name = driver.find_element_by_xpath('/html/body/section[2]/div/div[4]/div[{}]/div[2]/div/div[1]/div/div/h5'.format(counter)).text
             # Find the designer's patron number by using counter and formatting it to the XPath
